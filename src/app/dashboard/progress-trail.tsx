a72ebb4d189e3bf613e6254a053cdd024dac9ec2
Aldemir S <aldxmir.s2@gmail.com>
2025-09-04 20:03:11 +0000

    Detectar elementos que causam scroll horizontal.
    Redimensionar todos os

--- src/app/dashboard/progress-trail.tsx
+++ src/app/dashboard/progress-trail.tsx
@@ -195,8 +195,10 @@ export default function ProgressTrail() {
                         );
                     })}
                 </CarouselContent>
-                <CarouselPrevious className="hidden md:flex left-0" />
-                <CarouselNext className="hidden md:flex right-0" />
+                 <div className="absolute top-1/2 -translate-y-1/2 left-0 right-0 h-0 overflow-visible">
+                    <CarouselPrevious className="hidden md:flex left-0" />
+                    <CarouselNext className="hidden md:flex right-0" />
+                </div>
                 </Carousel>
             </CardContent>
         </Card>
